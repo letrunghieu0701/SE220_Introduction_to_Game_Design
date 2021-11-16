c40553e4bd17e3aa84492df5e8d8598631bd0fc3
nguyentrungnghia2000 <nguyentrungnghia.2000@gmail.com>
2021-11-16 15:43:18 +0700

    movement

--- Assets/Scripts/Knight.cs
+++ Assets/Scripts/Knight.cs
@@ -5,62 +5,117 @@ using UnityEngine;
 public class Knight : MonoBehaviour
 {
     public float moveSpeed;
+    public float jumpForce;
+    bool _canMoveLeft;
+    bool _canMoveRight;
     Rigidbody2D knight_rb;
     Animator knight_ani;
     GamePadsController gamePadsController;
-    
     bool isDead;
+    bool facingRight = true;
+
+    private bool isGround;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+    private int extraJumps;
+    public int extraJumpValue;
+    bool canJump;
+
+    public bool CanMoveLeft { get => _canMoveLeft; set => _canMoveLeft = value; }
+    public bool CanMoveRight { get => _canMoveRight; set => _canMoveRight = value; }
 
     private void Awake(){
-        knight_rb = GetComponent<Rigidbody2D>();
+        knight_rb = transform.GetComponent<Rigidbody2D>();
         knight_ani = GetComponent<Animator>();
     }
 
     private void FixedUpdate(){
+        if(isGround){
+            if(knight_ani){
+                knight_ani.SetBool("jump", false);
+            }
+        } else {
+            if(knight_ani){
+                knight_ani.SetBool("jump", true);
+            }
+        }
+
         MoveHandle();
     }
 
     void MoveHandle(){
-        if(gamePadsController != null){
-            if(gamePadsController.CanMoveLeft){
-                if(knight_rb){
-                    knight_rb.velocity = new Vector2(-1, knight_rb.velocity.y) * moveSpeed;
-                }
-
-                if(knight_ani){
-                    knight_ani.SetBool("walk", true);
-                }
+        if(Input.GetKey(KeyCode.LeftArrow)){
+            knight_rb.velocity = new Vector2(-moveSpeed, knight_rb.velocity.y);
+            if(facingRight){
+                Flip();
             }
-            else if(gamePadsController.CanMoveRight){
-                if(knight_rb){
-                    knight_rb.velocity = new Vector2(1, knight_rb.velocity.y) * moveSpeed;
-                }
-
-                if(knight_ani){
-                    knight_ani.SetBool("walk", true);
-                }
-            }
-            else{
-                if(knight_rb){
-                    knight_rb.velocity = new Vector2(0f, knight_rb.velocity.y);
-                }
-
-                if(knight_ani){
-                    knight_ani.SetBool("walk", false);
-                }
+            if(knight_ani){
+                knight_ani.SetBool("walk", true);
             }
         }
-        
+        else if(Input.GetKey(KeyCode.RightArrow)){
+            knight_rb.velocity = new Vector2(+moveSpeed, knight_rb.velocity.y);
+            if(!facingRight){
+                Flip();
+            }
+            if(knight_ani){
+                knight_ani.SetBool("walk", true);
+            }
+        }
+        else{
+            if(knight_rb){
+                knight_rb.velocity = new Vector2(0f, knight_rb.velocity.y);
+            }
+            if(knight_ani){
+                knight_ani.SetBool("walk", false);
+            }
+        }
+    }
+
+    void Flip(){
+        facingRight = !facingRight;
+        Vector3 scaler = transform.localScale;
+        scaler.x *= -1;
+        transform.localScale = scaler;
+    }
+
+    private bool IsGround(){
+        isGround = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+        return isGround;
+    }
+
+    void Dead(){
+        if(knight_ani){
+            knight_ani.SetTrigger("die");
+            isDead = true;
+        }
+
+        if(knight_rb){
+            knight_rb.velocity = new Vector2(0f, knight_rb.velocity.y);
+        }
+    }
+
+    private void OnCollision2D(Collision2D col){
+        if(col.gameObject.CompareTag("enemy")){
+            if(isDead) return;
+        }
     }
 
     void Start()
     {
-        GameObject obj = new GameObject();
-        obj.AddComponent<GamePadsController>();
-        gamePadsController = obj.GetComponent<GamePadsController>();
+        extraJumps = extraJumpValue;
     }
 
     void Update()
     {
+        _canMoveLeft = Input.GetAxis("Horizontal") < 0;
+        _canMoveRight = Input.GetAxis("Horizontal") > 0;
+
+        if( IsGround() && Input.GetKeyDown(KeyCode.Space)){
+            if(isGround){
+                knight_rb.velocity = Vector2.up * jumpForce;
+            }
+        }
     }
 }
